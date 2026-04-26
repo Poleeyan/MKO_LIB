2b042dc07baadfd168c08cf5f2b3a284ae7e310a
Poleeyan <v0va12yt@gmail.com>
2026-04-26 22:13:41 +0300

    lab8+

--- MKO_LIB/lab8.cs
+++ MKO_LIB/lab8.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Text;
 
 namespace MKO_LIB
@@ -8,46 +9,38 @@ namespace MKO_LIB
         public static string Run()
         {
             var output = new StringBuilder();
-
-            output.AppendLine("=== Лабораторна робота 8 ===");
-            output.AppendLine("=== Побудова сплайнів третього порядку ===\n");
+            output.Append("""
+                === Лабораторна робота 8 ===
+                === Побудова сплайнів третього порядку ===
+                """);
 
             try
             {
                 // Табличні дані
                 double[] X = { 3, 6, 9, 12 };
                 double[] Y = { 12, 16, 10, 21 };
+                double[] testPoints = { 0.5, 1.5, 2.5 }; // Точки для знаходження наближеного значення функції
 
                 output.AppendLine("Вхідні дані (таблиця):");
-                for (int i = 0; i < X.Length; i++)
-                {
-                    output.Append($"{X[i],5} ");
-                }
-                output.AppendLine();
-                for (int i = 0; i < Y.Length; i++)
-                {
-                    output.Append($"{Y[i],5} ");
-                }
-                output.AppendLine("\n");
+                
+                output.AppendLine(string.Join(" ", X.Select(x => $"{x,5}")));
+                output.AppendLine(string.Join(" ", Y.Select(y => $"{y,5}")));
+                output.Append('\n');
 
-                // Точки для знаходження наближеного значення функції
-                double[] testPoints = { 0.5, 1.5, 2.5 };
-
-                foreach (var xTarget in testPoints)
+                foreach (var xTarget in testPoints) // Обчислення кубічним сплайном
                 {
-                    // Обчислення кубічним сплайном
                     double splineResult = CubicSplineInterpolationMethod.Calculate(X, Y, xTarget);
-
-                    output.AppendLine($"Шукана точка X* = {xTarget}");
-                    output.AppendLine($"Наближене значення Y*: {splineResult:F4}");
-                    output.AppendLine(new string('-', 40));
+                    output.AppendLine($"""
+                        Шукана точка X* = {xTarget}
+                        Наближене значення Y*: {splineResult:F4}
+                        {new string('-', 40)}
+                        """);
                 }
             }
             catch (Exception ex)
             {
                 output.AppendLine($"Помилка: {ex.Message}");
             }
-
             return output.ToString();
         }
     }
